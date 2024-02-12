df34bb84e1d077ef937d830857ef638157756e1e
Elikibs <elishakibet67@gmail.com>
2024-02-12 11:41:52 +0300

    update js

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -5,15 +5,17 @@ let hasBlackjack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20){
-    message = "Do you want to draw a new card?"
+function startGame(){
+    if (sum <= 20){
+        message = "Do you want to draw a new card?"
+    }
+    else if (sum === 21){
+        message = "Wohoo! You've got Blackjack!"
+        hasBlackjack = true
+    }
+    else{
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    console.log(message);
 }
-else if (sum === 21){
-    message = "Wohoo! You've got Blackjack!"
-    hasBlackjack = true
-}
-else{
-    message = "You're out of the game!"
-    isAlive = false
-}
-console.log(message);
