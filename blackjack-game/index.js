1814cbbaaa1f1cdd4f9ffa3fb4afa8ab307396ed
Elikibs <elishakibet67@gmail.com>
2024-02-12 10:00:08 +0300

    update js

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,13 +1,17 @@
 let firstCard = 5
 let secondCard = 5
 let sum = firstCard + secondCard
+let hasBlackjack = false
+let isAlive = true
 
 if (sum < 20){
     console.log("Do you want to draw a new card?")
 }
 else if (sum === 21){
     console.log("Wohoo! You've got Blackjack!")
+    hasBlackjack = true
 }
 else{
     console.log("You're out of the game!");
+    isAlive = false
 }
