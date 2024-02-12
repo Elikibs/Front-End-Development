780538e1940881f94ab45082704294517e256dad
Elikibs <elishakibet67@gmail.com>
2024-02-12 09:04:53 +0300

    update js file

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,3 +1,13 @@
-firstCard = 5
-secondCard = 6
-sum = firstCard + secondCard
+let firstCard = 5
+let secondCard = 5
+let sum = firstCard + secondCard
+
+if (sum < 20){
+    console.log("Do you want to draw a new card?")
+}
+else if (sum === 21){
+    console.log("Wohoo! You've got Blackjack!")
+}
+else{
+    console.log("You're out of the game!");
+}
