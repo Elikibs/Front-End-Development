886f1d65b66e272a8d37d8852fec97b843bea67b
Elikibs <elishakibet67@gmail.com>
2024-02-12 17:00:26 +0300

    update js

--- blackjack-game/index.js
+++ blackjack-game/index.js
@@ -1,16 +1,38 @@
-let firstCard = 5
-let secondCard = 20
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10){
+        return 10
+    } else if (randomNumber === 1){
+        return 11
+    } else{
+        return randomNumber
+    }
+}
+
 function startGame(){
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent += sum
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame(){
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++){
+        cardsEl.textContent += cards[i] + " "
+    }
+    sumEl.textContent =  "Sum:" + sum
 
     if (sum <= 20){
         message = "Do you want to draw a new card?"
@@ -26,6 +48,11 @@ function startGame(){
     messageEl.textContent = message
 }
 
-function newcard() {
-    console.log("Drawing a new card from the deck!");
+function newCard() {
+    if (isAlive == true && hasBlackjack == false) {
+        let card = getRandomCard()
+        cards.push(card)
+        sum += card
+        renderGame()
+    }
 }
